3230e1fc3dba88b99202ef066f7ad2c06f618400
kaichi <hellokitty321ds@gmail.com>
2023-03-27 17:59:11 +0800

    WIP

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang='zh-CN'>
+      <Head>
+        <link href='/favicons/favicon.ico' rel='shortcut icon' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
