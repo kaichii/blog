040f2831e01e1d3249d08551c4ccb35ddbc7b626
kaichi <hellokitty321ds@gmail.com>
2023-03-30 10:56:24 +0800

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'next-themes';
 import 'react-notion-x/src/styles.css';
 import 'styles/global.css';
 import 'styles/notion.css';
+import 'styles/prism.css';
 
 export default function APP({ Component, pageProps }: AppProps) {
   return (
