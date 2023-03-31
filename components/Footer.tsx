3edd8a0d126950fa628c863e4d51b7e76d89ad28
kaichi <hellokitty321ds@gmail.com>
2023-03-31 17:47:37 +0800

    Add Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import { Github, Twitter } from './Icons';
 import styles from './styles.module.css';
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <Link
+          className={styles.footerLink}
+          target='_blank'
+          href='https://github.com/kaichii'
+        >
+          <Github />
+        </Link>
+        <Link
+          className={styles.footerLink}
+          target='_blank'
+          href='https://twitter.com/k41chi'
+        >
+          <Twitter />
+        </Link>
+      </div>
+      <div className={styles.copyright}>
+        <p>© {new Date().getFullYear()} KAI CHI.</p>
+      </div>
+    </footer>
+  );
 }
