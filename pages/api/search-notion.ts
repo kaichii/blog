798aabaa8e835d6493bda16b3344ab76f047780b
kaichii <contact@kaichi.dev>
2023-05-04 10:32:00 +0000

    Filter Un-Published Article

--- pages/api/search-notion.ts
+++ pages/api/search-notion.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import * as types from '../../lib/types';
 import { search } from '../../lib/notion';
+import { getPageProperty } from 'notion-utils';
+import remove from 'lodash.remove';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
@@ -11,6 +13,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const searchParams: types.SearchParams = req.body;
 
   const results = await search(searchParams);
+
+  results.results.forEach(item => {
+
+    const block = results.recordMap?.block?.[item.id]?.value;
+
+    const publishedTime = getPageProperty<number>('Published', block, results.recordMap as types.ExtendedRecordMap);
+
+    if (!publishedTime) remove(results.results, (result) => result.id === item.id);
+  })
 
   res.setHeader(
     'Cache-Control',
