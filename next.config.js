8d48248dd4aba4272c0593c17823fff9a25d2787
kaichi <contact@kaichi.dev>
2023-07-04 19:36:27 +0800

    Fix: Assets Error

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'kaichi.dev',
+      "www.notion.so",
+      "notion.so",
+      "images.unsplash.com",
+      "pbs.twimg.com",
+      "abs.twimg.com",
+      "s3.us-west-2.amazonaws.com",
+      "blog.kaichi.dev",
     ],
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
